e8f8335d5fbf8c8b2365e245813f9c1ffac87181
PhucDuong-SSS <phucdnDev@gmail.com>
2021-05-04 20:14:00 +0700

    [add] directive props

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <p :class="['todo-item', todoProps.isComplete ?'isCompleted':'']">  <input type="checkbox" :checked="todoProps.isComplete"/>
+    {{todoProps.title}}
+    <button class="del-btn">Delete</button>
+    </p>
+</template>
+
+<script>
+export default {
+    name: 'TodoItem',
+    props: ['todoProps']
+}
+</script>
+
+<style>
+    .del-btn{
+        background: #ff0000;
+        color: #fff;
+        border: none;
+        cursor: pointer;
+        float: right;
+    }
+    .todo-item{
+        background: #f4f4f4;
+        padding: 10px;
+        margin: 0;
+        border: 1px #ccc dotted;    
+    }
+    .isCompleted {
+       text-decoration: line-through; 
+        }
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,39 @@
+<template>
+  <TodoItem v-for="todo in todos" v-bind:key="todo.id" v-bind:todoProps="todo" />   
+  
+</template>
+<script>
+import {ref} from 'vue'
+import TodoItem from './TodoItem'
+export default {
+    name: 'Todos',
+    components : {TodoItem},
+    setup()
+    {
+        const todos = ref([
+            {
+                id: 1,
+                title: 'viec1',
+                isComplete: false,
+        },
+            {
+                id: 2,
+                title: 'viec2',
+                isComplete: false,
+        },
+            {
+                id: 3,
+                title: 'viec3',
+                isComplete: true,
+        }
+        ])
+        return {
+            todos
+        }    
+    }
+}
+</script>
+
+<style>
+
+</style>
